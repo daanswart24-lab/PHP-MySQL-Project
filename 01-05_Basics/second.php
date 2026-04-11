9c9ff56461d5e5d52f413b3f724ed24f5761c24f
Daan Swart <daanswart24@gmail.com>
2026-04-11 16:37:22 +0200

    Add second.php with basic HTML structure and PHP echo example

---
+++ 01-05_Basics/second.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Using Echo</title>
+</head>
+<body>
+    <!-- Script 1.3 - second.php --> 
+    <p>This is standard HTML</p>
+    <?php
+    echo 'This was generated using PHP';
+    ?>
+</body>
+</html>
