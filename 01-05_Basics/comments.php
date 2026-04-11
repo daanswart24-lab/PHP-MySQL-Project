5ab0a971d45179ddd2ef07fa67cbc7da8b07b2fb
Daan Swart <daanswart24@gmail.com>
2026-04-11 16:52:26 +0200

    Add comments.php with basic HTML structure and PHP echo examples

---
+++ 01-05_Basics/comments.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comments in PHP</title>
+</head>
+<body>
+    <?php
+
+    # Script 1.4 - comments.php
+    # Created April 11, 2026
+    # Created by Daan Swart
+    # This script does nothing much.
+    
+    echo "<p>This is a line of text.<br>This is another line of text.</p>";
+
+    /*
+    This line will not be executed, because it is inside a multi-line comment.
+    */
+
+    echo "<p>Now I'm done.</p>";
+    //End of PHP code.
+
+    ?>
+</body>
+</html>
