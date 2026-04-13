0941eda5345d87cba94b56d8991c15d353514ffd
Daan Swart <daanswart24@gmail.com>
2026-04-13 16:39:22 +0200

    Add strings.php with HTML structure and PHP variable examples

---
+++ 01-05_Basics/strings.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strings</title>
+</head>
+<body>
+    <?php # Script 1.6 - strings.php 
+    
+    // Create the variables:
+    $first_name = 'Haruki';
+    $last_name = 'Murakami';
+    $book = 'Kafka on the Shore';
+
+    // Print the values:
+    echo "<p>The book <em>$book</em> was written by $first_name $last_name.</p>";
+
+    ?>
+</body>
+</html>
