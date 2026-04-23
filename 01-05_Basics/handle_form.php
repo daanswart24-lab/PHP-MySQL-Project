8956b42d0301188cf6faac00e7f89e3a4e357d60
Daan Swart <daanswart24@gmail.com>
2026-04-23 13:52:38 +0200

    Add gender handling in handle_form.php to personalize feedback messages

--- 01-05_Basics/handle_form.php
+++ 01-05_Basics/handle_form.php
@@ -16,13 +16,27 @@
     $email = $_REQUEST['email'];
     $comments = $_REQUEST['comments'];
 
+    // Check if the gender value is present in the $_REQUEST array:
+    if (isset($_REQUEST['gender'])) {
+        $gender = $_REQUEST['gender'];
+    } else {
+        $gender = NULL;
+    }
+
     // Print the received email, name, and comment values:
     echo "<p>Thank you, <b>$name</b>, for the following comments:
         <br /><tt>$comments</tt></p>
         <p>We will reply to you at <i>$email</i>.</p>\n";
-
+    
+    // Print a message based on the gender value:
+    if ($gender == 'M') {
+        echo "<p><b>Good day, Sir!</b></p>";
+    } elseif ($gender == 'F') {
+        echo "<p><b>Good day, Madam!</b></p>";
+    } else {
+        echo "<p><b>You forgot to enter your gender!</b></p>";
+    }
     ?>
-
 </body>
 
 </html>
