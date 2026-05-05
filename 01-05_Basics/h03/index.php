3c9f50d65f69b0ae05d0c6a9a64cc03b09996562
Daan Swart <daanswart24@gmail.com>
2026-05-05 13:48:53 +0200

    Add header, footer, and index files for basic site structure

---
+++ 01-05_Basics/h03/index.php
@@ -0,0 +1,20 @@
+<?php # Script 3.4 - index.php
+$page_title = 'Welcome to this Site!';
+include ('includes/header.html');
+?>
+
+<h1>Content Header</h1>
+<p>This is where the page-specific content goes. This section, and the corresponding header, 
+    will change from one page to the next.
+</p>
+
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus exercitationem non sequi, 
+    molestias quasi quaerat dolore voluptates perspiciatis qui odit neque quisquam, 
+    enim mollitia illum cumque totam laborum animi est, rem itaque inventore? Sunt eum, 
+    quae esse eos itaque atque iusto facilis 
+    non qui fuga eaque maiores blanditiis at voluptate.</p>
+
+<?php 
+include ('includes/footer.html');
+?>
+
